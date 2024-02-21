266d2c28895c0e202a83c40bf086193b4bd5fbf8
Albert <dow.albert@gmail.com>
2024-02-21 20:13:51 +0000

    feat: first integration test and endpint

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+#[tokio::test]
+async fn health_check_works() {
+    // arange
+    spawn_app();
+    // need reqwest
+    let client = reqwest::Client::new();
+
+    // act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    //assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
